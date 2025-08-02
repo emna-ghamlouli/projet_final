7babbb4e61fd471d8d8673de095763672bfda1e6
emna ghamlouli <52451850+emna-ghamlouli@users.noreply.github.com>
2025-08-02 21:03:49 +0200

    Update CustomerController.java

--- demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://frontend.local")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
